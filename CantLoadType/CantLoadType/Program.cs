d9e83480e328b3b0893d0bf9392b0daa13f806f7
Kirill Osenkov <github@osenkov.com>
2017-11-20 17:15:02 -0800

    Add CantLoadType repro

---
+++ CantLoadType/CantLoadType/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace CantLoadType
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var assembly = Assembly.Load("Microsoft.CodeAnalysis.EditorFeatures");
+            var types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                Reflect(type);
+            }
+        }
+
+        private static void Reflect(Type type)
+        {
+            foreach (var property in type.GetProperties())
+            {
+
+            }
+        }
+    }
+}
