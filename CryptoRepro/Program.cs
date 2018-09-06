d8c531f4bfcd5178f2accf6f7b153fc0103f076b
Kirill Osenkov <github@osenkov.com>
2018-09-06 11:36:40 -0700

    Add CryptoRepro.

---
+++ CryptoRepro/Program.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System.Reflection;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var assembly = Assembly.Load("Microsoft.CodeAnalysis.Workspaces");
+        var checksum = assembly.GetType("Microsoft.CodeAnalysis.Checksum");
+        var create = checksum.GetMethod("Create", new[] { typeof(Stream) });
+    }
+}
