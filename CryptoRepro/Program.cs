3fdd1907774d82b34b31258177db66c76b788409
Kirill Osenkov <github@osenkov.com>
2018-09-06 11:45:47 -0700

    Actually call the method.

--- CryptoRepro/Program.cs
+++ CryptoRepro/Program.cs
@@ -8,5 +8,6 @@ class Program
         var assembly = Assembly.Load("Microsoft.CodeAnalysis.Workspaces");
         var checksum = assembly.GetType("Microsoft.CodeAnalysis.Checksum");
         var create = checksum.GetMethod("Create", new[] { typeof(Stream) });
+        var result = create.Invoke(null, new object[] { new MemoryStream() });
     }
 }
